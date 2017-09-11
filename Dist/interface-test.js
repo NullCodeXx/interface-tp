c33da8321e8f9bb4293ab11244cb7844b8176c20
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-11 20:54:15 +0200

    edit argument of the class dog

--- Dist/interface-test.js
+++ Dist/interface-test.js
@@ -4,8 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Saver_1 = require("./Saver");
 var chien_1 = require("./chien");
 var saver = new Saver_1.Saver();
-saver.save(new chien_1.Chien('toto', 'tata')); //on peut l'utiliser a plusieur reprise.
-saver.save(new chien_1.Chien('titi', 'tutu'));
+saver.save(new chien_1.Chien('Milo', 'jack ressel')); //on peut l'utiliser a plusieur reprise.
+saver.save(new chien_1.Chien('Nikita', 'staff america'));
+saver.save(new chien_1.Chien('Rex', 'rot'));
 saver.display();
 //Supprimer une valeur de l'array.
 saver.delete(1);
--- interface-test.ts
+++ interface-test.ts
@@ -1,12 +1,12 @@
 // 4 On test et appel les func avec des valeur concrete. (node Dist/interface-test).
 
 import {Saver} from './Saver';
-import {Savable} from './interface-save';
 import {Chien} from './chien';
 
 const saver = new Saver();
-saver.save(new Chien('toto', 'tata')); //on peut l'utiliser a plusieur reprise.
-saver.save(new Chien('titi', 'tutu'));
+saver.save(new Chien('Milo', 'jack ressel')); //on peut l'utiliser a plusieur reprise.
+saver.save(new Chien('Nikita', 'staff america'));
+saver.save(new Chien('Rex', 'rot'));
 saver.display();
 //Supprimer une valeur de l'array.
 saver.delete(1);
